44bda98d3d09504d0d91b7f24520aa1a683359c3
Sam <samjopp@googlemail.com>
2019-05-12 22:33:23 +0100

    Changing getter and setter to Data

--- src/main/java/com/jopp/customerservice/entity/Customer.java
+++ src/main/java/com/jopp/customerservice/entity/Customer.java
@@ -1,13 +1,13 @@
 package com.jopp.customerservice.entity;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
 
-@Getter
-@Setter
+@Data
 @Entity
 @Table(name = "customer")
 public class Customer {
--- src/main/java/com/jopp/customerservice/io/GetCustomerResponse.java
+++ src/main/java/com/jopp/customerservice/io/GetCustomerResponse.java
@@ -2,11 +2,12 @@ package com.jopp.customerservice.io;
 
 import com.jopp.customerservice.model.Customer;
 import com.jopp.customerservice.model.Info;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+
+@Data
 public class GetCustomerResponse {
 
     private Customer customer;
